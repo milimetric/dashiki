bf6fe74fcebb5eb2d583c79b412ffe931ba29622
milimetric <dandreescu@wikimedia.org>
2014-07-25 09:59:07 -0400

    Clean up code not for wikimania

--- src/app/startup.js
+++ src/app/startup.js
@@ -1,19 +1,4 @@
-define(['jquery', 'knockout', './router', 'bootstrap', 'knockout-projections', './bindings'], function($, ko, router) {
-
-  // Components can be packaged as AMD modules, such as the following:
-  ko.components.register('nav-bar', { require: 'components/nav-bar/nav-bar' });
-  ko.components.register('home', { require: 'components/home/home' });
-
-  // ... or for template-only components, you can just point to a .html file directly:
-  ko.components.register('about', {
-    template: { require: 'text!components/about/about.html' }
-  });
-
-  ko.components.register('error', { require: 'components/error/error' });
-
-  ko.components.register('vega-graph', { require: 'components/vega-graph/vega-graph' });
-
-  ko.components.register('dashboard', { require: 'components/dashboard/dashboard' });
+define(['jquery', 'knockout', 'knockout-projections', './bindings'], function($, ko) {
 
   ko.components.register('wikimetrics-data-browser', { require: 'components/wikimetrics-data-browser/wikimetrics-data-browser' });
 
@@ -24,5 +9,5 @@ define(['jquery', 'knockout', './router', 'bootstrap', 'knockout-projections', '
   // [Scaffolded component registrations will be inserted here. To retain this feature, don't remove this comment.]
 
   // Start the application
-  ko.applyBindings({ route: router.currentRoute });
+  ko.applyBindings();
 });
